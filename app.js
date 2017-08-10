7939c5b4e75d871b90c99e8ca9eccb7f8d35cc61
Marcelo Ferreira <joamarcelo@gmail.com>
2017-08-10 09:32:56 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/produtos', function(req, res){
+    res.render('produtos/lista');
+});
+
+app.listen(3000, function(){
+    console.log("Servidor rodando");
+});
+
