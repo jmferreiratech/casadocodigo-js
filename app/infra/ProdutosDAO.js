cd6b636c224f92709dc8cd8f3f9341c6474d835e
Marcelo Ferreira <joamarcelo@gmail.com>
2017-08-11 14:51:36 -0300

    Implements new book post

--- app/infra/ProdutosDAO.js
+++ app/infra/ProdutosDAO.js
@@ -6,6 +6,10 @@ ProdutosDAO.prototype.lista = function (callback) {
     this._connection.query('select * from produtos', callback);
 };
 
+ProdutosDAO.prototype.salva = function (produto, callback) {
+    this._connection.query('insert into produtos set ?', produto, callback);
+};
+
 module.exports = function () {
     return ProdutosDAO;
 };
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -11,4 +11,14 @@ module.exports = function (app) {
     app.get('/produtos/novo', function (req, res) {
         res.render('produtos/form');
     });
+
+    app.post('/produtos/novo', function (req, res) {
+        var produto = req.body;
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto, function (err, results) {
+            res.redirect('/produtos');
+        });
+        connection.end();
+    })
 };
