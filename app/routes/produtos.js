8757264e0e5cca59c4d80c22cc5ca95d74014fe1
Marcelo Ferreira <joamarcelo@gmail.com>
2017-08-14 07:58:52 -0300

    Adds validation with express-validator

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -4,19 +4,39 @@ module.exports = function (app) {
         var produtosDAO = new app.infra.ProdutosDAO(connection);
         produtosDAO.lista(function (err, results) {
             res.format({
-                html: function() {res.render('produtos/lista', {lista: results});},
-                json: function() {res.json(results);},
+                html: function () {
+                    res.render('produtos/lista', {lista: results});
+                },
+                json: function () {
+                    res.json(results);
+                },
             });
         });
         connection.end();
     });
 
     app.get('/produtos/novo', function (req, res) {
-        res.render('produtos/form');
+        res.render('produtos/form', {errosValidacao : {}, produto: {}});
     });
 
     app.post('/produtos/novo', function (req, res) {
         var produto = req.body;
+
+        req.assert('titulo', 'Titulo é obrigatório').notEmpty();
+        req.assert('preco', 'Formato inválido').isFloat();
+        var erros = req.validationErrors();
+        if (erros) {
+            res.format({
+                html: function() {
+                    res.status(400).render('produtos/form', {errosValidacao : erros, produto: produto});
+                },
+                json: function () {
+                    res.status(400).json(erros);
+                },
+            });
+            return;
+        }
+
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
         produtosDAO.salva(produto, function (err, results) {
--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var load = require('express-load');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 module.exports = function () {
     var app = express();
@@ -10,6 +11,7 @@ module.exports = function () {
 
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
+    app.use(expressValidator());
 
     load('routes', {cwd: 'app'})
         .then('infra')
