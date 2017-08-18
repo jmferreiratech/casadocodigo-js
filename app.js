63c7910a9db9a5e9f678537b7a8100dad4a81a1a
Marcelo Ferreira <joamarcelo@gmail.com>
2017-08-18 19:21:20 -0300

    Prepares to deploy

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var io = require('socket.io')(http);
 
 app.set('io', io);
 
-http.listen(3000, function () {
-    console.log("servidor rodando");
+var porta = process.env.PORT || 3000;
+var server = http.listen(porta, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+
 });
--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -17,6 +17,16 @@ function connectionFactory() {
             database: "casadocodigo_nodejs_test",
         });
     }
+    if (process.env.NODE_ENV === 'production') {
+        const urlConnection = process.env.CLEARDB_DATABASE_URL;
+        const params = urlConnection.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
+        return mysql.createConnection({
+            host: params[3],
+            user: params[1],
+            password: params[2],
+            database: params[4],
+        });
+    }
 }
 
 module.exports = function () {
